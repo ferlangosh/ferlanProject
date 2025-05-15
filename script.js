de18f285735df2a485f3f9c8774d229a37bc1b42
ferlangosh <ferlan.novendi@gmail.com>
2025-05-15 11:07:33 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Simple Project loaded successfully!');
+    
+    // Add event listeners or other functionality here
+});
